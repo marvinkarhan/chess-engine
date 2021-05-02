7318e612691c06e09b5ba492a010c1ced134ec3c
Marvin Karhan <1821400@stud.hs-mannheim.de>
2021-05-02 16:45:36 +0200

    update class variables

--- backend/test.py
+++ backend/test.py
@@ -8,11 +8,12 @@ class TestFullGame(unittest.TestCase):
 
     def test_shortest_game(self):
         board = Board()
-        board.make_move(uci_to_Move("f2f3"))
-        board.make_move(uci_to_Move("e7e6"))
+        board.parse_FEN_string(START_POS_FEN)
+        board.make_move(uci_to_Move('f2f3'))
+        board.make_move(uci_to_Move('e7e6'))
 
-        board.make_move(uci_to_Move("g2g4"))
-        board.make_move(uci_to_Move("d8h4"))
+        board.make_move(uci_to_Move('g2g4'))
+        board.make_move(uci_to_Move('d8h4'))
 
         self.assertTrue(board.checkmate())
 
