e9f01190b790f372917f1eff7b5349e110086af8
Joel Staubach <lightningstreamlp@gmail.com>
2021-05-02 15:25:55 +0200

    Mod improvments for deployment script

--- backend/start.ps1
+++ backend/start.ps1
@@ -1,3 +1,7 @@
+Write-Output "Installing Flask"
+pip install flask
+Write-Output "Installing Flask-Socket-IO"
+pip install flask-socketio
 # Set the enivroment variables for the api server.
 $env:FLASK_APP = "api.py"
 # Start the flask instance.
