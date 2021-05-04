c8c975b4e749bc65d3fc33ca91bbc61563d8ef96
Joel Staubach <lightningstreamlp@gmail.com>
2021-05-04 11:43:23 +0200

    Add api board function

--- backend/api.py
+++ backend/api.py
@@ -1,5 +1,9 @@
+from constants import START_POS_FEN
+from flask.globals import request, session
+from move_helper import uci_to_Move
+from board import Board
 from flask import Flask, render_template
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, emit, send
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -13,9 +17,22 @@ def onMessage(message: str):
 def on_test_message(message: str):
     print("Test-Message: " + message);
 
+@socketio.on('get_board_moves')
+def on_get_board_moves(fen: str):
+    user_board = session[request.sid]
+    moves = list(user_board.legal_moves_generator(user_board.active_side))
+    uci_moves = [move.to_uci_string() for move in moves]
+
+    print(uci_moves)
+
 @socketio.on('connect')
 def on_connect(): 
     print('Created new Socket-Connection')
+
+@socketio.on('new_board') 
+def on_new_board():
+    session[request.sid] = Board(START_POS_FEN)
+    emit('board_initialized', START_POS_FEN)
 
 if __name__ == '__main__':
     socketio.run(app)
