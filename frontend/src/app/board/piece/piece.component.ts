c6938957b03ed66e72c6ae2d9a632f997fee67f0
Joel Staubach <lightningstreamlp@gmail.com>
2021-05-14 18:17:18 +0200

    Fix: Capture top left rook

--- frontend/src/app/board/piece/piece.component.ts
+++ frontend/src/app/board/piece/piece.component.ts
@@ -39,8 +39,7 @@ export class PieceComponent implements AfterViewInit {
 
   constructor(private boardAudioService: BoardAudioService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   ngAfterViewInit(): void {
     this.initDragAndDrop();
@@ -48,68 +47,61 @@ export class PieceComponent implements AfterViewInit {
 
   initDragAndDrop() {
     this.pieceEl.style.cursor = 'grab';
-    
-    let grabEvent = fromEvent<MouseEvent>(this.pieceEl, 'mousedown')
-      .pipe(
-        tap((event) => {
-          this.onDragStart.emit(this.pieceProperties.possibleTargetSquares);
-          this.centerElFromEventOnCursor(event);
-          this.pieceEl.style.cursor = 'grabbing';
-          this.pieceEl.style.zIndex = '1000';
-          const matrix = this.getMatrix();
-          // set current posistions
-          let pos = {
-            // The transform positions
-            left: +matrix[4],
-            top: +matrix[5],
-            // Get the current mouse position
-            x: event.clientX,
-            y: event.clientY,
-          };
-          // listen for mouse movement
-          fromEvent<MouseEvent>(document, 'mousemove')
-            .pipe(
-              // terminate when mouse is let go
-              takeUntil(
-                fromEvent<MouseEvent>(document, 'mouseup').pipe(take(1))
-              ),
-              tap((event) => {
-                // How far the mouse has been moved
-                const dx = event.clientX - pos.x;
-                const dy = event.clientY - pos.y;
 
-                // Scroll the element
-                this.setTranslate(pos.left + dx, pos.top + dy);
-              })
-            )
-            .subscribe();
-        }),
-        switchMap(() =>
-          // wait until mouse is releases
-          fromEvent<MouseEvent>(document, 'mouseup').pipe(take(1))
-        ),
-        tap(() => {
-          // TODO: check if move as legal
-          this.resetStyle();
-          if (!this.checkBounds()) {
-            this.setTranslate(
-              this.position.x * 100,
-              this.position.y * 100,
-              '%'
-            );
-          } else {
-            this.updatePiece();
-          }
-          this.onDragStop.emit();
-        })
-      )
-      grabEvent.subscribe()
+    let grabEvent = fromEvent<MouseEvent>(this.pieceEl, 'mousedown').pipe(
+      tap((event) => {
+        this.onDragStart.emit(this.pieceProperties.possibleTargetSquares);
+        this.centerElFromEventOnCursor(event);
+        this.pieceEl.style.cursor = 'grabbing';
+        this.pieceEl.style.zIndex = '1000';
+        const matrix = this.getMatrix();
+        // set current posistions
+        let pos = {
+          // The transform positions
+          left: +matrix[4],
+          top: +matrix[5],
+          // Get the current mouse position
+          x: event.clientX,
+          y: event.clientY,
+        };
+        // listen for mouse movement
+        fromEvent<MouseEvent>(document, 'mousemove')
+          .pipe(
+            // terminate when mouse is let go
+            takeUntil(fromEvent<MouseEvent>(document, 'mouseup').pipe(take(1))),
+            tap((event) => {
+              // How far the mouse has been moved
+              const dx = event.clientX - pos.x;
+              const dy = event.clientY - pos.y;
+
+              // Scroll the element
+              this.setTranslate(pos.left + dx, pos.top + dy);
+            })
+          )
+          .subscribe();
+      }),
+      switchMap(() =>
+        // wait until mouse is releases
+        fromEvent<MouseEvent>(document, 'mouseup').pipe(take(1))
+      ),
+      tap(() => {
+        // TODO: check if move as legal
+        this.resetStyle();
+        if (!this.checkBounds()) {
+          this.setTranslate(this.position.x * 100, this.position.y * 100, '%');
+        } else {
+          this.updatePiece();
+        }
+        this.onDragStop.emit();
+      })
+    );
+    grabEvent.subscribe();
   }
 
   private updatePiece() {
     let matrix = this.getMatrix();
     let [newX, newY] = [+matrix[4] / 100, +matrix[5] / 100];
-    this.boardAudioService.playMoveSound()
+    this.boardAudioService.playMoveSound();
     this.onMove.emit([this.position, { x: newX, y: newY }]);
     this.position.y = newY;
     this.position.x = newX;
@@ -124,14 +116,14 @@ export class PieceComponent implements AfterViewInit {
   private checkBounds() {
     const matrix = this.getMatrix();
     let [newX, newY] = [+matrix[4] / 100, +matrix[5] / 100];
+    console.log('newX,Y', newX, newY);
+    const newPositionNumber = newX + newY * 8;
     let found = this.pieceProperties.possibleTargetSquares.find(
       (positionNumber: number) => {
-        const newPositionNumber = newX + newY * 8;
-        if (positionNumber === newPositionNumber) return positionNumber;
-        else return undefined;
+        return positionNumber === newPositionNumber
       }
     );
-    return !!found;
+    return found != undefined;
   }
 
   private centerOnCell() {
