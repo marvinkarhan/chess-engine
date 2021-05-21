2ef6fe80d919c20e739c7119c6374fba290c66db
Joel Staubach <lightningstreamlp@gmail.com>
2021-05-21 18:25:03 +0200

    Mod Use other sockets for new backend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,9 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 import { BoardModule } from './board/board.module';
+import makeWebSocketObservable from 'rxjs-websockets';
 
-const config: SocketIoConfig = { url: 'http://localhost:5000', options: {} };
+const config: SocketIoConfig = { url: 'http://jjkkkkj:8000', options: {} };
+
+const socket = new WebSocket("ws://localhost:8000/ws")
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, BoardModule, SocketIoModule.forRoot(config)],
--- frontend/src/app/board/services/chess-api.service.ts
+++ frontend/src/app/board/services/chess-api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
-import { Observable } from 'rxjs';
+import { fromEvent, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { ChessApiEmits, ChessApiEvents } from '../enums/ChessApiEvents';
 import { BoardInformation } from '../interfaces/BoardInformation';
 import {
@@ -14,17 +15,30 @@ import { BoardService } from './board.service';
   providedIn: 'root',
 })
 export class ChessApiService {
-  constructor(private socket: Socket) {}
 
-  onNewBoardInformation(): Observable<BoardInformation> {
-    return this.socket.fromEvent<BoardInformation>(ChessApiEvents.NEW_BOARD_INFORMATION);
+  newSocket: WebSocket;
+  constructor(private socket: Socket) {
+    this.newSocket = new WebSocket("ws://localhost:8000/ws")
+  } 
+  
+  connect() {
+    return fromEvent(this.newSocket, 'open');
+  }
+  
+
+  onNewBoardInformation() : Observable<BoardInformation> {
+    return fromEvent<MessageEvent>(this.newSocket, 'message').pipe(
+      map((event: MessageEvent) => JSON.parse(event.data))
+    );
   }
 
   requestNewBoard() : void {
+    this.newSocket?.send(ChessApiEmits.NEW_BOARD);
     this.socket.emit(ChessApiEmits.NEW_BOARD);
   }
 
   requestNewMove(uciMove: string): void {
+    this.newSocket?.send(ChessApiEmits.MAKE_MOVE);
     this.socket.emit(ChessApiEmits.MAKE_MOVE, uciMove);
   } 
 
