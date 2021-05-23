ef992d0f2452e072e36d9695387f088b3af43475
Joel Staubach <lightningstreamlp@gmail.com>
2021-05-23 18:09:18 +0200

    Add install script for windows

---
+++ new_backend/utility/install-modules.ps1
@@ -0,0 +1,43 @@
+Remove-Item -Recurse -Force tmp -ErrorAction SilentlyContinue
+
+Remove-Item -Recurse -Force src/vendor -ErrorAction SilentlyContinue
+mkdir tmp
+cd tmp
+
+$VENDOR = '../../../src/vendor/'
+## install oatpp
+
+$MODULE_NAME="oatpp"
+
+git clone --depth=1 https://github.com/oatpp/$MODULE_NAME
+
+cd $MODULE_NAME
+
+mkdir build
+cd build
+
+cmake -DCMAKE_INSTALL_PREFIX="$VENDOR"  ..
+cmake --build . --target INSTALL
+
+cd ../../
+
+##########################################################
+## install oatpp-websocket
+
+$MODULE_NAME="oatpp-websocket"
+
+git clone --depth=1 https://github.com/oatpp/$MODULE_NAME
+
+cd $MODULE_NAME
+mkdir build
+cd build
+
+cmake -DCMAKE_INSTALL_PREFIX="$VENDOR"  ..
+cmake --build . --target INSTALL
+
+cd ../../
+
+#End
+cd ../
+
+Remove-Item -Recurse -Force tmp -ErrorAction SilentlyContinue
