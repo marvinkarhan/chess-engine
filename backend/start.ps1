62e72c298ef1edd7e0fa7d93a5452a8f7fe1c64a
Marvin Karhan <1821400@stud.hs-mannheim.de>
2021-09-13 18:38:26 +0200

    Remove old python backend

--- backend/start.ps1
+++
@@ -1,9 +0,0 @@
-Write-Output "Installing Flask"
-pip install flask
-Write-Output "Installing Flask-Socket-IO"
-pip install flask-socketio
-# Set the enivroment variables for the api server.
-$env:FLASK_APP = "api.py"
-$env:FLASK_ENV = "development"
-# Start the flask instance.
-python -m flask run
