2685ceb966d3a45e3d9d7fb8fe2299241ff7d428
Marvin Karhan <1821400@stud.hs-mannheim.de>
2021-09-13 21:09:26 +0200

    update start.sh

--- new_backend/src/flask/start.sh
+++ new_backend/src/flask/start.sh
@@ -3,10 +3,12 @@ echo "Installing Flask"
 pip3 install flask
 echo "Installing Flask-Socket-IO"
 pip3 install flask-socketio
+echo "Installing Pexpect"
+pip3 install pexpect
 echo "Installing Eventlet"
 pip3 install eventlet==0.30.2
 echo "Installing Gunicorn"
 pip3 install gunicorn
 
 # run
-gunicorn -k eventlet -w 1 --reload app:app
+python3 -m gunicorn -k eventlet -w 1 --reload app:app
