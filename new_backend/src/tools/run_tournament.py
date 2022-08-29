34e32008c8d38b4a5f37105b70f76cc25e9b6872
Marvin Karhan <1821400@stud.hs-mannheim.de>
2022-08-29 21:55:11 +0200

    Add new tests

--- new_backend/src/tools/run_tournament.py
+++ new_backend/src/tools/run_tournament.py
@@ -6,18 +6,23 @@ DEFAULT_ENGINE_SAVE = '../build/bin/uci-engine'
 CLASSIC_ENGINE = '../../../../saved_engines/wsl/classic'
 STOCKFISH_WITH_MY_NET = './engines/stockfish_mse_55_epoch'
 OPENING_BOOK_EPD = './books/UHO_V3_6mvs_+090_+099.epd'
+NNUE_CENTROPY_111_EPOCH = './engines/centropy_111_epoch'
+STOCKFISH = './engines/stockfish_11_x64'
 
 # using cutechess (cli): https://github.com/cutechess/cutechess
 
 class Engine:
-  def __init__(self, file = DEFAULT_ENGINE_SAVE, name = ''):
+  def __init__(self, file = DEFAULT_ENGINE_SAVE, name = '', options=[]):
     self.file = file
     self.name = name
+    self.option = "".join([f'option.{o[0]}={o[1]} ' for o in options])
 
   def to_string(self):
     string = f' -engine cmd={self.file} '
     if self.name:
       string += f'name={self.name} '
+    if self.option:
+      string += self.option
     return string
 
 class Tournament:
@@ -35,15 +40,15 @@ class Tournament:
       f'-rounds {self.games} '
       f'-pgnout tournaments/{datetime.now().strftime("%d-%m-%Y_%H-%M-%S")}.pgn '
       f'-openings file={OPENING_BOOK_EPD} format=epd -repeat '
-      f'-concurrency 8'
+      f'-concurrency 6'
       # f'-draw movenumber=40 movecount=8 score=10 '
     )
     print(cmd)
     os.system(cmd)
 
 def main():
-  engines = [Engine(CLASSIC_ENGINE, name='CLASSIC'), Engine(name='NNUE')]
-  tournament = Tournament(engines, 1, 60, 1)
+  engines = [Engine(name='NNUE'), Engine(CLASSIC_ENGINE, name='HCE')]
+  tournament = Tournament(engines, 500, 60, 1)
   tournament.start()
 
 if __name__ == '__main__':
